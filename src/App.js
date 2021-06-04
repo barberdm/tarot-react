d18b6f323ed3ea02e99fbe7a4e05415cea5f8ba7
barberdm <81453773+barberdm@users.noreply.github.com>
2021-06-04 12:23:07 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/header'
+import Home from './components/home'
+import Services from './components/services'
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+    return(
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="homepage.html">Home</a> </li>
+                    <li><a href="services.html">Services</a> </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
